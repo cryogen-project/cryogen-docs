63a2aa97c30b2811f3e3b84a43fee779b8452c99
Carmen La <carmen.wla@gmail.com>
2017-02-01 23:26:24 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.7.0"]
-                           [ring/ring-devel "1.3.2"]
-                           [compojure "1.3.4"]
+            :dependencies [[org.clojure/clojure "1.8.0"]
+                           [ring/ring-devel "1.5.1"]
+                           [compojure "1.5.2"]
                            [ring-server "0.4.0"]
-                           [selmer "0.8.2"]
+                           [selmer "1.10.6"]
                            [clj-text-decoration "0.0.3"]
-                           [io.aviso/pretty "0.1.18"]
+                           [io.aviso/pretty "0.1.33"]
                            [cryogen-markdown "0.1.5"]
                            [cryogen-core "0.1.21"]]
             :plugins [[lein-ring "0.8.13"]]
