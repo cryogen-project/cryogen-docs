3299e999a7e8c7eb3dc08200d20378efa781c542
Dorab Patel <dorabpatel@gmail.com>
2020-10-16 19:55:44 -0700

    Bumped version to 0.1.1
    Also bumped up dependency versions.
    In particular, cryogen-core to 0.3.2 and cryogen-markdown to 0.1.13

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject cryogen "0.1.0"
+(defproject cryogen "0.1.1"
             :description "Simple static site generator"
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.8.0"]
+            :dependencies [[org.clojure/clojure "1.10.0"]
                            [ring/ring-devel "1.5.1"]
                            [compojure "1.5.2"]
                            [ring-server "0.4.0"]
                            [selmer "1.10.6"]
                            [clj-text-decoration "0.0.3"]
                            [io.aviso/pretty "0.1.33"]
-                           [cryogen-markdown "0.1.5"]
-                           [cryogen-core "0.1.21"]]
+                           [cryogen-markdown "0.1.13"]
+                           [cryogen-core "0.3.2"]]
             :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
