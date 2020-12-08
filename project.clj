072826541dd6c34e57b6845fc9025127dd0a9dd3
Dorab Patel <dorabpatel@gmail.com>
2020-12-08 09:44:31 -0800

    Bump version to 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen "0.1.3"
+(defproject cryogen "0.1.4-SNAPSHOT"
             :description "Simple static site generator"
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
